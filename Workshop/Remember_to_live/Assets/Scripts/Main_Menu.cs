4fcf33f5acb48e81b29afece538012ee5be325af
ssitero <42685018+ssitero@users.noreply.github.com>
2019-10-02 09:21:42 -0400

    Main_Menu
    Basic Menu with new game functionality

---
+++ Workshop/Remember_to_live/Assets/Scripts/Main_Menu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_Menu : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    public void PlayGame (){
+      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKey("escape")){
+          Application.Quit();
+        }
+    }
+}
