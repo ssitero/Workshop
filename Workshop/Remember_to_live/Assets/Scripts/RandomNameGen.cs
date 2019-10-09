b2920bde2d154f0467c9194fdf20ab8668908969
Yingying <37191990+milchbrotchen@users.noreply.github.com>
2019-10-09 10:34:20 -0400

    Menu Canvas and Random Name Gen (WIP)
    Changed Main menu canvas settings and in progress adding hard coded list of names for each character based on player name input.

--- Workshop/Remember_to_live/Assets/Scripts/RandomNameGen.cs
+++ Workshop/Remember_to_live/Assets/Scripts/RandomNameGen.cs
@@ -1,28 +1,76 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RandomNameGen : MonoBehaviour
 {
     [SerializeField]
-    private string[] randomNameList;
+    private string[26][5] randomNameList;
 
-    void Awake()
+    private string playerName;
+
+    void Start()
     {
-        randomNameList = new string[]
+        // A names
+        randomNameList[0] = new string[]
         {
             "Adrian",
-            "Brian",
-            "Karen",
-            "Melissa",
-            "Frances",
             "Abcde",
-            "Susan",
+            "Alyssa",
+            "Aaron",
+            "Abel",
+        };
+
+        // B names
+        randomNameList[1] = new string[]
+        {
+            "Bethany",
+            "Bruce",
+            "Barbara",
+            "Bonnie",
+            "Brian",
+        };
+
+        // C names
+        randomNameList[2] = new string[]
+        {
+            "Caleb",
+            "Chris",
+            "Cara",
+            "Calamari",
+            "Caleb",
+        };
+
+        // D names
+        randomNameList[3] = new string[]
+        {
+            "Dorian",
+            "Deedee",
+            "Dan",
+            "David",
+            "Diego",
+        };
+
+        // E names
+        randomNameList[4] = new string[]
+        {
+            "Adrian",
+            "Abcde",
+            "Alyssa",
+            "Aaron",
+            "Abel",
         };
     }
-	
-    public static string GetRandomName()
+
+    public string GetRandomName(string playerName)
     {
-        return randomNameList[Random.Range(0, randomNameList.Length)];
+        int letter = (int)'a' - playerName[0];
+        Debug.Log(letter);
+        return randomNameList[letter][Random.Range(0, 5)];
+    }
+
+    private void Update()
+    {
+        Debug.Log(GetRandomName("Boooooo"));
     }
 }
