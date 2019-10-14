007ca18b23e37d37c0532a11bab84740ea8d3d64
Yingying <37191990+milchbrotchen@users.noreply.github.com>
2019-10-14 12:16:51 -0400

    10/14/19 Class Work
    More work to take home TT

---
+++ Workshop/Remember_to_live/Assets/Scripts/Battle/Enemy_StateMachine.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_StateMachine : MonoBehaviour
+{
+  public Enemy_BattleStats enemy;
+  private Battle_StateMachine BSM;
+
+  public enum TurnState
+  {
+    Processing,
+    ChooseAction,
+    Wait,
+    Action,
+    Dead
+  }
+
+  public TurnState enemyState;
+
+  public GameObject selector;
+
+  private bool alive = true;
+
+  void Start()
+  {
+    selector.SetActive(false);
+    enemyState = TurnState.Processing;
+    BSM = GameObject.Find("BattleManager").GetComponent<Battle_StateMachine>();
+  }
+
+  void Update()
+  {
+    Debug.Log(enemyState);
+    switch (enemyState)
+    {
+      case (TurnState.Processing):
+        // ????
+        break;
+
+      case (TurnState.ChooseAction):
+        if (!BSM.playerIsAlive)
+        {
+          Debug.Log("Player dead");
+          break;
+        }
+        else
+        {
+          chooseAction();
+          enemyState = TurnState.Wait;
+        }
+        break;
+
+      case (TurnState.Wait):
+        break;
+
+      case (TurnState.Action):
+        StartCoroutine(enemyAction());
+        break;
+
+      case () //WORKING ON THIS
+
+    }
+  }
+
+  void chooseAction()
+  {
+
+  }
+
+
+}
