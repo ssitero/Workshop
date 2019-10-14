2a5d59326ee373cc0610c3eab4e7bf080f68c293
Yingying <yingyingzhang15@gmail.com>
2019-10-14 18:56:39 -0400

    Proof of Concept Programming Done
    UI WIP

---
+++ Workshop/Remember_to_live/Assets/Scripts/Battle/Base Objs/Enemy_BattleStats.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Enemy_BattleStats
+{
+    // Enemy stats
+    public string enemyName;
+
+    public float enemyHP, curHP;
+    public float playerMP, curMP;
+
+    public int enemyStr;
+    public int enemyInt;
+    public int enemySpd;
+
+    // Enemy Type
+    public enum Type
+    {
+        Conversational,
+        Nonsentient,
+        Monster
+    }
+
+    public Type enemyType;
+
+    public List<Attack_Base> attacks = new List<Attack_Base>();
+
+}
---
+++ Workshop/Remember_to_live/Assets/Scripts/Battle/Player_Panel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Player_Panel : MonoBehaviour
+{
+    public TextMeshProUGUI playerName;
+    public TextMeshProUGUI playerHP;
+    public TextMeshProUGUI playerMP;
+
+    public Image progressbar;
+}
